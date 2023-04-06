68772c5bc8996e742208209ac6b3b6fe1bca827e
Neeraj <singhneeraj03@gmail.com>
2023-04-06 15:02:23 +0530

    1st commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test_db',
+    entities: ["src//*.entity.ts", "dist//*entity.ts"],
+    synchronize: true,
+    autoLoadEntities: true,
+  }),UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
